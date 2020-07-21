4870bd7b8b1d36e472c82a74883c259ce0aff015
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-07-20 22:00:26 -0300

    First  Commit on GITHUB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+// PrimeNG Modules
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    InputTextModule    
   ],
   providers: [],
   bootstrap: [AppComponent]
