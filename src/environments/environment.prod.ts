17d0fc669f7a28d1dd59b2c8df0cacaf8a257590
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-08-08 14:30:44 -0300

    Final version deploy on Heroku 2.3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'http://rlspmoney-api.heroku.com',
+  apiURL: 'https://rlspmoney-api.heroku.com',
 
   allowedDomaninsProd: [ new RegExp('rlspmoney-api.herokuapp.com')],
   disallowedRoutesProd: [ new RegExp('\/oauth\/token') ]
