3478320a65acbd396056b688392f1317986e1de6
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-08-09 00:28:21 -0300

    fixed enviroment.prod 'apiURL'

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname + '/dist//rlspmoney-ui'));
+app.use(express.static(__dirname + '/dist/rlspmoney-ui'));
 
 app.get('/*', function(req, res) {
   res.sendFile(__dirname + '/dist//rlspmoney-ui/index.html');
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiURL: 'https://rlspmoney-api.herokuapp.com/',
+  apiURL: 'https://rlspmoney-api.herokuapp.com',
 
   allowedDomaninsProd: [ new RegExp('rlspmoney-api.herokuapp.com')],
   disallowedRoutesProd: [ new RegExp('\/oauth\/token') ]
